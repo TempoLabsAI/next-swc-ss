5f0d60f7ff1f2a8930aa5a07dd08e36813fa825a
Daniel Whitney <tiedye1@hotmail.com>
2026-01-08 16:52:41 -0500

    upgrade next js (#5)

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { updateSession } from "./supabase/middleware";
 import { type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   return await updateSession(request);
 }
 
